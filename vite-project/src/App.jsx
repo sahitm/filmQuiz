4e3052618aac65dbd3342a92b1073b8f1072e0bb
sahitm <sahit7varma@gmail.com>
2022-07-19 19:44:53 +0530

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='bg-slate-400'>
+        hello world
+    </div>
+    </div>
+  )
+}
+
+export default App
